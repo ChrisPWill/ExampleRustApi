bfe72eb5df19f107b246093d34b89d59ecab49dd
Chris Williams <chrispwill@gmail.com>
2017-03-22 22:20:49 +1100

    Create api

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+use rocket::http::Status;
+
+#[get("/speak/<value>")]
+fn speak(value: u32) -> Result<String, Status> {
+    match robot_speak(value) {
+        Some(result) => Ok(result),
+        None => Err(Status::NotFound),
+    }
+}
+
+fn main() {
+    rocket::ignite().mount("/v0/", routes![speak]).launch();
+}
+
+/// Produces commentary on the bleakness of robotic existence, all wrapped
+/// nicely as an `Option<String>`.
+///
+/// # Examples
+///
+/// ```
+/// let purpose = robot_speak(42);
+/// println!("{}", purpose);
+/// ```
+pub fn robot_speak(x: u32) -> Option<String> {
+    match x {
+        42 => Some("What is my purpose?".to_string()),
+        100...200 => Some("Beep boop".to_string()),
+        _ => None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use robot_speak;
+
+    #[test]
+    fn test_purpose() {
+        assert_eq!(robot_speak(42), Some("What is my purpose?".to_string()));
+    }
+
+    #[test]
+    fn test_boop() {
+        assert_eq!(robot_speak(150), Some("Beep boop".to_string()));
+    }
+
+    #[test]
+    fn test_other() {
+        assert_eq!(robot_speak(343), None);
+    }
+}
